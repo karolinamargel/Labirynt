d2279a757dd5257b3ae6d4d94aca6c9d00ce94a9
karolinamargel <karolina.margel@vp.pl>
2015-12-03 09:26:38 +0100

    pierwsza plansza

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,33 @@
 #include<allegro5\allegro_ttf.h>
 #include<allegro5\allegro_native_dialog.h>
 #include<allegro5\allegro_image.h>
+#include<fstream>
+
+#define RozmiarBloku 16
+
+int licznikX = 0, licznikY = 0, rozmiarX, rozmiarY;
+int mapa[100][100];
+
+void WczytajMape(const char *nazwa, int mapa[][100]){
+	std::ifstream openfile(nazwa);
+	if (openfile.is_open()){
+		openfile >> rozmiarX >> rozmiarY;
+		while (!openfile.eof()){
+			openfile >> mapa[licznikX][licznikY];
+			licznikX++;
+
+			if (licznikX >= rozmiarX){
+				licznikX = 0;
+				licznikY++;
+			}
+		}
+	}
+	else{
+
+	}
+}
+
+void RysujMape(int mapa[][100]);
 
 int main(void)
 {
@@ -12,10 +39,10 @@ int main(void)
 
 	bool done = false;
 	bool redraw = true;
-	int pos_x = width / 2;
-	int pos_y = height / 2;
+
 	int FPS = 60;
 	int wroc = 0;
+	int licz = 0;
 
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
@@ -40,12 +67,14 @@ int main(void)
 	al_init_ttf_addon();
 	al_init_image_addon(); 
 
-	ALLEGRO_BITMAP *bitmapa = al_load_bitmap("lab.jpeg");
+	ALLEGRO_BITMAP *menu = al_load_bitmap("lab.jpeg");
 	ALLEGRO_FONT *font = al_load_font("FORTE.TTF", 80, 0);
 	ALLEGRO_FONT *font1 = al_load_font("FORTE.TTF", 30, 0);
+	ALLEGRO_FONT *font2 = al_load_font("FORTE.TTF", 25, 0);
 	ALLEGRO_BITMAP *plansza01 = al_load_bitmap("lab00.jpg");
 	ALLEGRO_BITMAP *wybor_planszy = al_load_bitmap("lab02.jpg");
 	ALLEGRO_BITMAP *pomoc = al_load_bitmap("labirynt2.jpg");
+	WczytajMape("plansza1.txt", mapa);
 
 
 	timer = al_create_timer(1.0 / FPS);
@@ -58,7 +87,7 @@ int main(void)
 
 	while (!done){
 		if (redraw){
-			al_draw_bitmap(bitmapa, 0, 0, 0);
+			al_draw_bitmap(menu, 0, 0, 0);
 			al_draw_textf(font, al_map_rgb(26, 255, 241), 130, 100, 0, "LABIRYNT");
 			al_draw_textf(font1, al_map_rgb(26, 255, 241), 130, 200, 0, "Start gry");
 			al_draw_textf(font1, al_map_rgb(26, 255, 241), 130, 230, 0, "Wybor planszy");
@@ -71,7 +100,6 @@ int main(void)
 		al_wait_for_event(event_queue, &ev);
 
 		if (ev.type == ALLEGRO_EVENT_KEY_DOWN && ev.keyboard.keycode == ALLEGRO_KEY_BACKSPACE){
-			
 			if (wroc == 1){
 				redraw = true;
 				wroc = 0;
@@ -81,7 +109,11 @@ int main(void)
 			done = true;
 		//start gry
 		else if (ev.mouse.x >= 130 && ev.mouse.x <= 300 && ev.mouse.y >= 200 && ev.mouse.y <= 230 && ev.mouse.button == 1){
-			al_draw_bitmap(plansza01,0,0,0);
+			al_draw_bitmap(plansza01, 0, 0, 0);
+			RysujMape(mapa);
+			al_draw_textf(font2, al_map_rgb(26, 255, 241), 5, 250, 0, "Start");
+			al_draw_textf(font2, al_map_rgb(26, 255, 241), 450, 10, 0, "Meta");
+
 			wroc = 1;
 			al_flip_display();
 		}
@@ -97,7 +129,6 @@ int main(void)
 			al_draw_bitmap(pomoc, 0, 0, 0);
 			wroc = 1;
 			al_flip_display();
-			
 		}
 		//wyjscie z gry
 		else if (ev.mouse.x >= 130 && ev.mouse.x <= 300 && ev.mouse.y >= 290 && ev.mouse.y <= 320 && ev.mouse.button == 1)
@@ -106,7 +137,7 @@ int main(void)
 		al_flip_display();
 	}
 
-	if (!bitmapa) {
+	if (!menu) {
 		al_show_native_message_box(display, "Blad", NULL,
 			"Nie moge wczytac bitmapy !", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		return 0;
@@ -120,7 +151,18 @@ int main(void)
 	al_destroy_timer(timer);
 	al_destroy_event_queue(event_queue);
 	al_destroy_display(display);
-	al_destroy_bitmap(bitmapa); 
+	al_destroy_bitmap(menu); 
 
 	return 0;
+}
+
+void RysujMape(int mapa[][100]){
+	for (int i = 0; i < rozmiarX; i++){
+		for (int j = 0; j < rozmiarY; j++){
+			if (mapa[i][j] == 0)
+				al_draw_filled_rectangle(i*RozmiarBloku, j*RozmiarBloku, i*RozmiarBloku + RozmiarBloku, j*RozmiarBloku + RozmiarBloku, al_map_rgba_f(0, 0, 255, 0.3));
+			else
+				al_draw_filled_rectangle(i*RozmiarBloku, j*RozmiarBloku, i*RozmiarBloku + RozmiarBloku, j*RozmiarBloku + RozmiarBloku, al_map_rgb(0, 255, 0));
+		}
+	}
 }
